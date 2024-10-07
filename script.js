16829c4dff0fcc577e5a94045aa835f8f1f221ca
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-10-07 21:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//your JS code here. If required.
+
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
+
 const express = require('express');
 const path = require('path');
 
